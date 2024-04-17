704b3ef0f912b1a9b88f146ad1aa49d340809dfd
bananenfreak2002 <167305180+bananenfreak2002@users.noreply.github.com>
2024-04-17 21:42:51 +0200

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -1,23 +1,28 @@
-const { SlashCommandBuilder } = require('discord.js');
-const { getVoiceConnection } = require('@discordjs/voice')
-
-module.exports = {
-    data: new SlashCommandBuilder()
-        .setName('leave')
-        .setDescription('Get the discord bot to leave the vc it is currently in <>_<>'),
-
-    async execute(interaction) {
-
-        const channel = interaction.options.getChannel('channel');
-
-        let connection = getVoiceConnection(interaction.guild.id);
-        if (connection === undefined) {
-            await interaction.reply("I am not in a vc!");
-            return;
-        };
-    
-        connection.destroy();
-
-        await interaction.reply(`Successfully left ${channel}!`);
-    },
-};
+client.on('message', async message => {
+    const args = message.content.slice(prefix.length).trim().split(/ +/);
+    const command = args.shift().toLowerCase();
+    if (command === 'leave') {
+             let authorVoice = message.member.voice; 
+        const botIsInSameChannel = client.voice.connections.some(
+  (c) => c.channel.id === authorVoice.channel.id,
+); 
+     const embed1 = new discord.MessageEmbed()
+      .setTitle(':no_entry_sign: You are not in a Voice Channel!')
+      .setColor('RED')
+      .setTimestamp();
+        if(!authorVoice.channel) return message.channel.send(embed1);
+        
+    const embed2 = new discord.MessageEmbed()
+      .setTitle(':no_entry: We are not in a same Voice Channel')
+      .setColor('YELLOW')
+      .setTimestamp();
+        if (!botIsInSameChannel) return message.channel.send(embed2)      
+        
+        const embed = new discord.MessageEmbed()
+      .setTitle('Successfully left the Voice Channel✔')
+      .setColor('RED')
+      .setTimestamp();
+        authorVoice.channel.leave();
+        message.channel.send(embed);
+      }
+    });
