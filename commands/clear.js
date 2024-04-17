ac98e3956372465c4c1dfdb79bcd4db5c14ab18c
bananenfreak2002 <167305180+bananenfreak2002@users.noreply.github.com>
2024-04-17 14:29:07 +0200

    Update clear.js

--- commands/clear.js
+++ commands/clear.js
@@ -19,7 +19,7 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "clear",
-  description: "Clears the music queue.",
+  description: "Löscht die Musikwarteschlange.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -28,11 +28,11 @@ module.exports = {
     
     try {
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ Es wird keine Musik gespielt!!', ephemeral: true });
       }
 
       if (!queue.songs[0]) {
-        return interaction.reply({ content: '❌ Queue is empty!!', ephemeral: true });
+        return interaction.reply({ content: '❌ Die Warteschlange ist leer!!', ephemeral: true });
       }
 
       await queue.stop(interaction.guild.id);
@@ -40,11 +40,11 @@ module.exports = {
       const embed = new EmbedBuilder()
         .setColor('#3498db')
         .setAuthor({
-          name: 'Cleared List',
+          name: 'Liste gelöscht',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157314241393598585/4618-no-slides.png?ex=65182861&is=6516d6e1&hm=dac8fed5a18e1574485e833d4c017591c50f59d161e1bde7fed5f6a92543f951&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          url: 'https://discord.gg/duesseldorf'
         })
-        .setDescription('**Queue cleared! Be Ready for a new musical journey.**')
+        .setDescription('**Warteschlange gelöscht! Seien Sie bereit für eine neue musikalische Reise.**')
        
 
       interaction.reply({ embeds: [embed] });
