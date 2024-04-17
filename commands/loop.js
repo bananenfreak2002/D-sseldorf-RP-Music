08970aa917afbdeda30bfc6eab11a6e9b179c176
bananenfreak2002 <167305180+bananenfreak2002@users.noreply.github.com>
2024-04-17 13:46:42 +0200

    Update loop.js

--- commands/loop.js
+++ commands/loop.js
@@ -1,3 +1,4 @@
+
 /*
 
   ██████╗░████████╗██╗░░██╗           
@@ -16,7 +17,7 @@
 const db = require("../mongoDB");
 module.exports = {
   name: "loop",
-  description: "Schaltet den Musik-Loop-Modus ein oder aus.",
+  description: "Turns the music loop mode on or off.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -25,7 +26,7 @@ module.exports = {
     try {
       const { EmbedBuilder, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require('discord.js');
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: '⚠️ Es wird keine Musik gespielt!!', ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true }).catch(e => { })
   
       let button = new ActionRowBuilder().addComponents(
         new ButtonBuilder()
@@ -45,11 +46,11 @@ module.exports = {
       const embed = new EmbedBuilder()
         .setColor('#fc4e03')
         .setAuthor({
-        name: 'Loopen Sie Ihre Melodien',
+        name: 'Loop Your Melodies',
         iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157318080670728283/7905-repeat.gif?ex=65182bf4&is=6516da74&hm=9ae58f40fcea5dc42a2a992bbd159d50116b3bafe5c5f7728e3a5276442efd2a&', 
         url: 'https://discord.gg/FUEHs7RCqz'
     })
-        .setDescription('**Looping! Lass die Musik immer weiter spielen. **')
+        .setDescription('**Looping it! Let the music play on and on. **')
      
       interaction?.reply({ embeds: [embed], components: [button], fetchReply: true }).then(async Message => {
 
@@ -60,27 +61,27 @@ module.exports = {
           if (button.user.id !== interaction.user.id) return
           const queue1 = client.player.getQueue(interaction.guild.id);
           if (!queue1 || !queue1.playing) {
-            await interaction?.editReply({ content: '⚠️ Es wird keine Musik gespielt!!', ephemeral: true }).catch(e => { })
+            await interaction?.editReply({ content: '⚠️ No music playing!!', ephemeral: true }).catch(e => { })
             await button?.deferUpdate().catch(e => {})
           }
           switch (button.customId) {
             case 'queue':
               const success = queue.setRepeatMode(2);
-              interaction?.editReply({ content: `✅ Schleifenwarteschlange!!` }).catch(e => { })
+              interaction?.editReply({ content: `✅ Looping Queue!!` }).catch(e => { })
               await button?.deferUpdate().catch(e => {})
               break
             case 'nowplaying':
               const success2 = queue.setRepeatMode(1);
-              interaction?.editReply({ content: `✅ Looping aktiviert!!` }).catch(e => { })
+              interaction?.editReply({ content: `✅ Looping activated!!` }).catch(e => { })
               await button?.deferUpdate().catch(e => {})
               break
             case 'close':
               if (queue.repeatMode === 0) {
                 await button?.deferUpdate().catch(e => {})
-                return interaction?.editReply({ content: '⚠️ Looping bereits ausgeschaltet!!', ephemeral: true }).catch(e => { })
+                return interaction?.editReply({ content: '⚠️ Looping already Off!!', ephemeral: true }).catch(e => { })
               }
               const success4 = queue.setRepeatMode(0);
-              interaction?.editReply({ content: '▶️ Looping aus' }).catch(e => { })
+              interaction?.editReply({ content: '▶️ Looping off' }).catch(e => { })
               await button?.deferUpdate().catch(e => {})
               break
           }
@@ -95,7 +96,7 @@ module.exports = {
 
           const embed = new EmbedBuilder()
             .setColor('#fc5203')
-            .setTitle('▶️ Looping aus!!')
+            .setTitle('▶️ Looping off!!')
             .setTimestamp()
 
           await interaction?.editReply({ content: "", embeds: [embed], components: [button] }).catch(e => { });
