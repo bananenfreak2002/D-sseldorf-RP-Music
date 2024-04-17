818f7480cf583c7de3646e552770db6ab02f95aa
bananenfreak2002 <167305180+bananenfreak2002@users.noreply.github.com>
2024-04-17 14:16:25 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -22,7 +22,7 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
 setInterval(() => client.user.setActivity({ 
-  name: `Düsseldorf Music Update v0.7 BETA`, 
+  name: `Update v0.7 ALPHA`, 
   type: ActivityType.Listening }), 10000);
 client.errorLog = config.errorLog
 } else {
