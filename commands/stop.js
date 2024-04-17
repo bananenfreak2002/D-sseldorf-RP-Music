f161f79bc1bce1107a1dbc4e1e475803dc28b667
bananenfreak2002 <167305180+bananenfreak2002@users.noreply.github.com>
2024-04-17 17:19:41 +0200

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -1,66 +1,25 @@
-/*
-
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-*/
-const db = require("../mongoDB");
-const { EmbedBuilder } = require('discord.js');
-
-module.exports = {
-  name: "stop",
-  description: "Stoppt die Musik",
-  permissions: "0x0000000000000800",
-  options: [],
-  voiceChannel: true,
-  run: async (client, interaction) => {
-    try {
-      const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ Derzeit spielt keine Musik!!', ephemeral: true });
-      }
-
-      queue.stop(interaction.guild.id);
-
-      const embed = new EmbedBuilder()
-        .setColor('#f1002c')
-        .setAuthor({
-          name: 'Musik gestoppt',
-          iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157305318255116400/pngtree-vector-stop-icon-png-image_4233262.jpg?ex=65182011&is=6516ce91&hm=d5a8ca6010716bae836b025f8d36557a95f14c13a705f65eb09a54161649c795&',
-          url: 'https://discord.gg/duesseldorf'
-        })
-        .setDescription('**Die Musik wurde beendet. Düsseldorf lebt weiter xD**')
-        
-
-      return interaction.reply({ embeds: [embed] });
-    } catch (e) {
-      console.error(e);
+run: async (client, interaction) => {
+  try {
+    const queue = client.player.getQueue(interaction.guild.id);
+    if (!queue || !queue.playing) {
+      return interaction.reply({ content: '⚠️ Derzeit spielt keine Musik!!', ephemeral: true });
     }
-  },
-};
 
+    // Stoppe die Wiedergabe und lasse den Bot den Voice-Channel verlassen
+    queue.stop(interaction.guild.id);
+    await queue.connection.disconnect();
 
-/*
+    const embed = new EmbedBuilder()
+      .setColor('#f1002c')
+      .setAuthor({
+        name: 'Musik gestoppt',
+        iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157305318255116400/pngtree-vector-stop-icon-png-image_4233262.jpg?ex=65182011&is=6516ce91&hm=d5a8ca6010716bae836b025f8d36557a95f14c13a705f65eb09a54161649c795&',
+        url: 'https://discord.gg/duesseldorf'
+      })
+      .setDescription('**Die Musik wurde beendet. Düsseldorf lebt weiter xD**');
 
-  ██████╗░████████╗██╗░░██╗           
-  ██╔══██╗╚══██╔══╝╚██╗██╔╝          
-  ██████╔╝░░░██║░░░░╚███╔╝░          
-  ██╔══██╗░░░██║░░░░██╔██╗░          
-  ██║░░██║░░░██║░░░██╔╝╚██╗          
-  ╚═╝░░╚═╝░░░╚═╝░░░╚═╝░░╚═╝          
-
-   
-   # MADE BY RTX!! FEEL FREE TO USE ANY PART OF CODE
-   ## FOR HELP CONTACT ME ON DISCORD
-   ## Contact    [ DISCORD SERVER :  https://discord.gg/FUEHs7RCqz ]
-   ## YT : https://www.youtube.com/channel/UCPbAvYWBgnYhliJa1BIrv0A
-*/
+    return interaction.reply({ embeds: [embed] });
+  } catch (e) {
+    console.error(e);
+  }
+},
