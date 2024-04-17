36f25118cba140b42d62bd267e627cff30eae73d
bananenfreak2002 <167305180+bananenfreak2002@users.noreply.github.com>
2024-04-17 13:54:35 +0200

    Update stop.js

--- commands/stop.js
+++ commands/stop.js
@@ -18,7 +18,7 @@ const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
   name: "stop",
-  description: "Stops the music.",
+  description: "Stoppt die Musik",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
@@ -26,7 +26,7 @@ module.exports = {
     try {
       const queue = client.player.getQueue(interaction.guild.id);
       if (!queue || !queue.playing) {
-        return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+        return interaction.reply({ content: '⚠️ Derzeit spielt keine Musik!!', ephemeral: true });
       }
 
       queue.stop(interaction.guild.id);
@@ -34,11 +34,11 @@ module.exports = {
       const embed = new EmbedBuilder()
         .setColor('#f1002c')
         .setAuthor({
-          name: 'Music Stopped',
+          name: 'Musik gestoppt',
           iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157305318255116400/pngtree-vector-stop-icon-png-image_4233262.jpg?ex=65182011&is=6516ce91&hm=d5a8ca6010716bae836b025f8d36557a95f14c13a705f65eb09a54161649c795&',
-          url: 'https://discord.gg/FUEHs7RCqz'
+          url: 'https://discord.gg/duesseldorf'
         })
-        .setDescription('**The journey stops, but the rhythm lives on.**')
+        .setDescription('**Die Musik wurde beendet. Düsseldorf lebt weiter xD**')
         
 
       return interaction.reply({ embeds: [embed] });
