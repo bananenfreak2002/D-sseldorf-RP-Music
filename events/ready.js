bd42bb167a5abd643d46067639c31c6b969e321b
bananenfreak2002 <167305180+bananenfreak2002@users.noreply.github.com>
2024-04-17 15:11:32 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -22,7 +22,7 @@ console.log('\x1b[36m%s\x1b[0m', '|    🚀 Commands Distracted!');
 console.log('\x1b[32m%s\x1b[0m', `|    🌼 Logged in as ${client.user.username}`);
 
 setInterval(() => client.user.setActivity({ 
-  name: `Update v0.7 ALPHA`, 
+  name: `Update V0.7 ALPHA`, 
   type: ActivityType.Listening }), 10000);
 client.errorLog = config.errorLog
 } else {
