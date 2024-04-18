e8d3fa0356805fc3d7f9d4e765963a12fda5f26a
bananenfreak2002 <167305180+bananenfreak2002@users.noreply.github.com>
2024-04-18 14:47:18 +0200

    Update skip.js

--- commands/skip.js
+++ commands/skip.js
@@ -17,11 +17,11 @@ const { ApplicationCommandOptionType, EmbedBuilder } = require('discord.js');
 const db = require("../mongoDB");
 module.exports = {
   name: "skip",
-  description: "Switches the music being played.",
+  description: "Schaltet die abgespielte Musik um.",
   permissions: "0x0000000000000800",
   options: [{
     name: "number",
-    description: "mention how many songs you wanna skip",
+    description: "Erwähnen Sie, wie viele Songs Sie überspringen möchten",
     type: ApplicationCommandOptionType.Number,
     required: false
   }],
@@ -31,27 +31,27 @@ module.exports = {
     try {
 
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: '⚠️ Es wird keine Musik gespielt!!', ephemeral: true }).catch(e => { })
 
       let number = interaction.options.getNumber('number');
       if (number) {
-        if (!queue.songs.length > number) return interaction.reply({ content: '⚠️ Exceeded current no of songs', ephemeral: true }).catch(e => { })
-        if (isNaN(number)) return interaction.reply({ content: '⚠️ Invalid Number', ephemeral: true }).catch(e => { })
-        if (1 > number) return interaction.reply({ content: '⚠️ Invalid Number', ephemeral: true }).catch(e => { })
+        if (!queue.songs.length > number) return interaction.reply({ content: '⚠️ Die aktuelle Anzahl an Songs wurde überschritten', ephemeral: true }).catch(e => { })
+        if (isNaN(number)) return interaction.reply({ content: '⚠️ Ungültige Nummer', ephemeral: true }).catch(e => { })
+        if (1 > number) return interaction.reply({ content: '⚠️ Ungültige Nummer', ephemeral: true }).catch(e => { })
 
         try {
         let old = queue.songs[0];
         await client.player.jump(interaction, number).then(song => {
-          return interaction.reply({ content: `⏯️ Skipped : **${old.name}**` }).catch(e => { })
+          return interaction.reply({ content: `⏯️ Übersprungen : **${old.name}**` }).catch(e => { })
         })
       } catch(e){
-        return interaction.reply({ content: '❌ Queue is empty!!', ephemeral: true }).catch(e => { })
+        return interaction.reply({ content: '❌ Die Warteschlange ist leer!!', ephemeral: true }).catch(e => { })
       }
       } else {
 try {
   const queue = client.player.getQueue(interaction.guild.id);
   if (!queue || !queue.playing) {
-    return interaction.reply({ content: '⚠️ No music playing!!', ephemeral: true });
+    return interaction.reply({ content: '⚠️ Es wird keine Musik gespielt!!', ephemeral: true });
   }
 
   let old = queue.songs[0];
@@ -60,16 +60,16 @@ try {
   const embed = new EmbedBuilder()
     .setColor('#3498db')
     .setAuthor({
-      name: 'Song Skipped',
+      name: 'Lied übersprungen',
       iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157269773118357604/giphy.gif?ex=6517fef6&is=6516ad76&hm=f106480f7d017a07f75d543cf545bbea01e9cf53ebd42020bd3b90a14004398e&',
-      url: 'https://discord.gg/FUEHs7RCqz'
+      url: 'https://discord.gg/duesseldorf'
     })
-    .setDescription(success ? ` **SKIPPED** : **${old.name}**` : '❌ Queue is empty!')
+    .setDescription(success ? ` **ÜBERSPRUNGEN** : **${old.name}**` : '❌ Die Warteschlange ist leer!')
     .setTimestamp();
 
   return interaction.reply({ embeds: [embed] });
 }catch (e) {
-          return interaction.reply({ content: '❌ Queue is empty!!', ephemeral: true }).catch(e => { })
+          return interaction.reply({ content: '❌ Die Warteschlange ist leer!!', ephemeral: true }).catch(e => { })
         }
       }
 
