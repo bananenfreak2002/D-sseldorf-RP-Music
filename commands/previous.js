231650f7bb6c8f8bb0aa9ee7032406f77e2d8269
bananenfreak2002 <167305180+bananenfreak2002@users.noreply.github.com>
2024-04-18 14:39:10 +0200

    Update previous.js

--- commands/previous.js
+++ commands/previous.js
@@ -1,19 +1,19 @@
 const db = require("../mongoDB");
 module.exports = {
   name: "previous",
-  description: "Plays the previous track.",
+  description: "Spielt den vorherigen Titel ab.",
   permissions: "0x0000000000000800",
   options: [],
   voiceChannel: true,
   run: async (client, interaction) => {
     try {
       const queue = client.player.getQueue(interaction.guild.id);
-      if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ No music playing!!`, ephemeral: true }).catch(e => { })
+      if (!queue || !queue.playing) return interaction.reply({ content: `⚠️ Es wird keine Musik gespielt!!`, ephemeral: true }).catch(e => { })
       try {
         let song = await queue.previous()
-        interaction.reply({ content: `**Behold, the enchanting melody of the past!!**` }).catch(e => { })
+        interaction.reply({ content: `**Seht, die bezaubernde Melodie der Vergangenheit!!**` }).catch(e => { })
       } catch (e) {
-        return interaction.reply({ content: `❌ No previous track!!`, ephemeral: true }).catch(e => { })
+        return interaction.reply({ content: `❌ Kein vorheriger Titel!!`, ephemeral: true }).catch(e => { })
       }
     } catch (e) {
     console.error(e); 
