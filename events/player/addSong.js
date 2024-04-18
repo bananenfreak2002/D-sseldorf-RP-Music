1ae4678dedcbac3a02b71af9c77636bb918d3fe0
bananenfreak2002 <167305180+bananenfreak2002@users.noreply.github.com>
2024-04-18 14:59:11 +0200

    Update addSong.js

--- events/player/addSong.js
+++ events/player/addSong.js
@@ -7,13 +7,13 @@ module.exports = async (client, queue, song) => {
     if (queue?.textChannel) {
       const embed = new EmbedBuilder()
         .setAuthor({
-        name: 'Added To Queue',
+        name: 'Zur Warteschlange hinzugefügt',
         iconURL: 'https://cdn.discordapp.com/attachments/1156866389819281418/1157218651179597884/1213-verified.gif?ex=6517cf5a&is=65167dda&hm=bc8fb4414cb412587ade0af285b77569d2568214cf7d6baab8702ddeb6c38ad5&', 
-        url: 'https://discord.gg/FUEHs7RCqz'
+        url: 'https://discord.gg/duesseldorf'
     })
         .setDescription(`<@${song.user.id}>, **${song.name}**`)
         .setColor('#14bdff')
-        .setFooter({ text: 'Use /queue for more Information' });
+        .setFooter({ text: 'Benutze /queue für mehr Informationen' });
       queue?.textChannel?.send({ embeds: [embed] }).catch(e => { });
     }
   }
